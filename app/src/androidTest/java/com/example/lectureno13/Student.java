8ee5e120fdd1411ace2e9e191dec0d962d93efb1
unknown <asmak9060@gamil.com>
2021-06-09 07:24:52 +0500

    2nd commit

--- app/src/androidTest/java/com/example/lectureno13/Student.java
+++ app/src/androidTest/java/com/example/lectureno13/Student.java
@@ -2,8 +2,11 @@ package com.example.lectureno13;
 public class Student {
     public int roll_No;
     public String name;
+    public int age;
+    public int getAge(){ return age;}
     public int getRoll_No(){ return roll_No;}
     public String getName(){ return name;}
+    public void setAge(int age){ this.age=age;}
     public void setRoll_No(int roll_No){ this.roll_No=roll_No;}
     public  void setName(String name){ this.name=name;}
 }
