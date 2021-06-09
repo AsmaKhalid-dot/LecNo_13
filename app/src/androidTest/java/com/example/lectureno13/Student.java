f96d435f8cd942427a648c4c4e72b9aa83c3eae0
unknown <asmak9060@gamil.com>
2021-06-09 06:23:29 +0500

    4th commit

---
+++ app/src/androidTest/java/com/example/lectureno13/Student.java
@@ -0,0 +1,10 @@
+package com.example.lectureno13;
+public class Student {
+    public int roll_No;
+    public String name;
+    public int getRoll_No(){ return roll_No;}
+    public String getName(){ return name;}
+    public void setRoll_No(int roll_No){ this.roll_No=roll_No;}
+    public  void setName(String name){ this.name=name;}
+}
+
